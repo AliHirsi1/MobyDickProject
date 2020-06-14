2dbfea9c5d324e131c97dc33fd2dd7e66653bc3a
Ali <Ali@LAPTOP-FUIB0GOV>
2020-06-13 22:47:12 -0500

    Added new Interface for counting frequency words

---
+++ MobyDick/MobyDick.Service/Interface/IFrequencyCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobyDick.Service.Interface
+{
+    public interface IFrequencyCount
+    {
+        List<string> GetAllFrequencyWordsExcludingStopWatchWords();
+        Dictionary<string, int> Top100FrequencyWords(List<string> words);
+
+        bool IsInStopWords(string word);
+    }
+}
