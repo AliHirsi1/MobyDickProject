33030cd4de1b8cc6773e431c09b7196a22ef115d
Ali <Ali@LAPTOP-FUIB0GOV>
2020-06-13 21:34:53 -0500

    Implemented FileReader that reads text file

---
+++ MobyDick.UI/MobyDick.Service/Interface/IFileReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobyDick.Service.Interface
+{
+    public interface IFileReader
+    {
+        List<string> MobyDickReader();
+        List<string> StopWordsReader();
+    }
+}
