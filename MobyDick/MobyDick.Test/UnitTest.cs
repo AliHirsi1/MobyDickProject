0600ab2dde561def9d5c071522df53722740f6a2
Ali <Ali@LAPTOP-FUIB0GOV>
2020-06-13 22:26:20 -0500

    Implemented fileReader and unit tested

---
+++ MobyDick/MobyDick.Test/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using MobyDick.Service.FileReader;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobyDick.Test
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        TextFileReader reader = new TextFileReader();
+
+        [Test]
+        public void MobyDickReader_ReadersTextFile()
+        {
+            var result = reader.MobyDickReader();
+            var title = "The Project Gutenberg EBook of Moby Dick;";
+            Assert.AreEqual(title, result[0] + " " + result[1] + " " + result[2] + " " + result[3] + " " + result[4] + " " + result[5] + " " + result[6]);
+        }
+
+
+    }
+}
